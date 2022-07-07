d49390eeffa454fb44ec78e0f4836953b249ec48
Helen Manders-Jones <realhelenmjones@gmail.com>
2022-07-07 16:02:49 +0100

    initial commit

---
+++ grails-app/controllers/bie/index/nbn/UrlMappings.groovy
@@ -0,0 +1,14 @@
+package bie.index.nbn
+
+class UrlMappings {
+
+    static mappings = {
+        "/$controller/$action?/$id?(.$format)?"{
+            constraints {
+                // apply constraints here
+            }
+        }
+
+
+    }
+}
---
+++ grails-app/init/bie/index/nbn/BootStrap.groovy
@@ -0,0 +1,9 @@
+package bie.index.nbn
+
+class BootStrap {
+
+    def init = { servletContext ->
+    }
+    def destroy = {
+    }
+}
