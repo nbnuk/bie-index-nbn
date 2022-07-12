5252db8cf398f7f95c083a21eb4845c404c20877
Helen Manders Jones <realhelenmjones@gmail.com>
2022-07-12 22:02:10 +0100

    extract SearchService.groovy

---
+++ grails-app/services/uk/org/nbn/bie/SearchService.groovy
@@ -0,0 +1,91 @@
+package uk.org.nbn.bie
+
+import au.org.ala.bie.search.IndexDocType
+import au.org.ala.bie.util.Encoder
+import grails.transaction.Transactional
+import groovy.json.JsonSlurper
+
+@Transactional
+class SearchService extends au.org.ala.bie.SearchService{
+
+    /**
+     * Retrieve details of a taxon by common name or scientific name
+     * NBN implementation.
+     * @param taxonID
+     * @return
+     */
+    @Override
+    protected def lookupTaxonByName(String taxonName, Boolean useOfflineIndex = false){
+        def indexServerUrlPrefix = grailsApplication.config.indexLiveBaseUrl
+        if (useOfflineIndex) {
+            indexServerUrlPrefix = grailsApplication.config.indexOfflineBaseUrl
+        }
+        def solrServerUrl = indexServerUrlPrefix + "/select?wt=json&q=" +
+                "commonNameExact:\"" + taxonName + "\" OR scientificName:\"" + taxonName + "\" OR exact_text:\"" + taxonName + "\"" + // exact_text added to handle case differences in query vs index
+                (grailsApplication.config?.solr?.bq ? "&" + grailsApplication.config.solr.bq + "&defType=dismax" : "") //use boosting if provided, since the first match will be selected which is otherwise fairly random
+
+        def queryResponse = new URL(Encoder.encodeUrl(solrServerUrl)).getText("UTF-8")
+        def js = new JsonSlurper()
+        def json = js.parseText(queryResponse)
+        json.response.docs[0]
+    }
+
+    /**
+     * Retrieve details of synonyms for a taxonID
+     *
+     * @param taxonID The taxon identifier
+     * @param useOfflineIndex
+     * @return
+     */
+    @Override
+    def lookupSynonyms(String taxonID, Boolean useOfflineIndex = false){
+        def indexServerUrlPrefix = useOfflineIndex ? grailsApplication.config.indexOfflineBaseUrl : grailsApplication.config.indexLiveBaseUrl
+        def encID = URLEncoder.encode(taxonID, 'UTF-8')
+
+        def synonymQueryUrl = indexServerUrlPrefix + "/select?wt=json&q=" +
+                "acceptedConceptID:\"" + taxonID + "\"" + "&fq=idxtype:" + IndexDocType.TAXON.name() +
+                "&sort=nameComplete+ASC&rows=200"
+
+        def queryResponse = new URL(synonymQueryUrl).getText("UTF-8")
+        def js = new JsonSlurper()
+        def json = js.parseText(queryResponse)
+        json.response.docs
+    }
+
+    @Override
+    def getTaxon(taxonLookup) {
+        def model = super.getTaxon(taxonLookup)
+
+        //exclude entries where label is empty
+        model.conservationStatuses.findAll{it.key != ""}
+
+        //sort=nameComplete+ASC"
+        model.synonyms.sort{it.nameComplete}
+
+        addOccurrenceCountsToTaxonModel(model, super.lookupTaxon(model.taxonConcept.guid))
+
+        model
+
+    }
+
+
+    protected void addOccurrenceCountsToTaxonModel(model, taxon) {
+        if (!taxon) {
+            return;
+        }
+
+        def docStats = [:]
+        if (taxon.containsKey("occurrenceCount")) {
+            docStats.put("occurrenceCount", taxon["occurrenceCount"])
+        }
+        def jsonSlurper = new JsonSlurper()
+        def AdditionalOccStats = jsonSlurper.parseText(grailsApplication.config?.additionalOccurrenceCountsJSON ?: "[]")
+        AdditionalOccStats.each { stats ->
+            if (taxon.containsKey(stats.solrfield)) {
+                docStats.put(stats.solrfield, taxon[stats.solrfield])
+            }
+        }
+
+        model.occurrenceCounts = docStats
+    }
+}
