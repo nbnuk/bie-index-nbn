0603e734197b906a21a0eab8239f89068797c0d3
Helen Manders Jones <realhelenmjones@gmail.com>
2022-08-16 09:01:08 +0100

    travis-nexus

--- src/test/groovy/uk/org/nbn/bie/PlacesAutoCompleteServiceSpec.groovy
+++ src/test/groovy/uk/org/nbn/bie/PlacesAutoCompleteServiceSpec.groovy
@@ -14,9 +14,4 @@ class PlacesAutoCompleteServiceSpec extends Specification {
 
     def cleanup() {
     }
-
-    void "test something"() {
-        expect:"fix me"
-            true == false
-    }
 }
